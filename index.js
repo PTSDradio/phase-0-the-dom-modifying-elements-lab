63b411a2e4d0d3594f36c19da714160998b8294a
Lawrence Nigel Anderson <nigel.lawrenceanderson@gmail.com>
2023-06-04 23:08:17 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const test = document.getElementById("main")
+test.remove();
+const newHeader = document.createElement("h1")
+
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Yomama is the champion!"
